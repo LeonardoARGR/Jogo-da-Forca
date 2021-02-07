358996c3ce7042c87ab488f8815953a0d8a4c793
Leonardo Aragão Gris <62727732+LeonardoARGR@users.noreply.github.com>
2021-02-07 19:52:38 -0300

    Melhoria no sistema de palavras
    Apenas melhorei o sistema de seleção de palavras, mas ele ainda vai receber outras atualizações.

--- Jogo da Forca/src/com/argr/main/Words.java
+++ Jogo da Forca/src/com/argr/main/Words.java
@@ -1,31 +1,47 @@
 package com.argr.main;
 
+import java.util.List;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Words {
 	
+	private List<String> lastWords;
+	private int wordCount = 0;
+	private int maxWords = 10;
+	private int maxCount = 0;
+	
 	public Words() {
+		lastWords = new ArrayList<String>();
 	}
 	
 	public String getWord(String path) {
 		String word = "";
 		File file = new File(path);
+		int count = 0;
 		if(file.exists()) {
 			try {
 				BufferedReader reader = new BufferedReader(new FileReader(file));
 				String line = null;
 				try {
+					maxCount++;
+					if(maxCount > maxWords) {
+						maxCount = 1;
+					}
 					while((line = reader.readLine()) != null) {
-						if(Game.rand.nextInt(100) < 30) {
-							word = reader.readLine();
-							System.out.println(word);
+						count++;
+						if(count == maxCount) {
+							word = line;
+							lastWords.add(word);
+							//System.out.println(lastWords);
 							return word;
 						}
+						
 					}
 				}catch(IOException e) {}
 				
